fa735d1692303188a2d5eb554702fa9271a152fe
이재현 <dlwogus1027@naver.com>
2023-10-15 15:33:17 +0900

    필요없는 파일 삭제

--- lib/features/store/navigation/menus/store_application_screen.dart
+++ lib/features/store/navigation/menus/store_application_screen.dart
@@ -1,15 +1,12 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:swag_marine_products/constants/gaps.dart';
 import 'package:swag_marine_products/constants/http_ip.dart';
 import 'package:swag_marine_products/models/database/order_model.dart';
-import 'package:swag_marine_products/models/store_order_model.dart';
 import 'package:swag_marine_products/providers/store_provider.dart';
-import 'package:swag_marine_products/widget_tools/swag_platform_dialog.dart';
 
 import 'package:http/http.dart' as http;
 
