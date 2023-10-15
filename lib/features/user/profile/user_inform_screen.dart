557e4d6fcec3961d35ced541c1d4a3a044705eba
이재현 <dlwogus1027@naver.com>
2023-10-15 15:40:37 +0900

    user의 home 페이지 제거 밑 오류 import 코드 제거

--- lib/features/user/profile/user_inform_screen.dart
+++ lib/features/user/profile/user_inform_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:swag_marine_products/constants/sizes.dart';
-import 'package:swag_marine_products/features/user/home/user_home_screen.dart';
 import 'package:swag_marine_products/features/sign_in_up/sign_in_screen.dart';
 import 'package:swag_marine_products/features/user/profile/user_inform_inquiry_screen.dart';
 import 'package:swag_marine_products/providers/user_provider.dart';
